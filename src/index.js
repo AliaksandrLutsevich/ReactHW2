2b786a550a1951524a5b1c552a0ae685c0c0cb85
24041997 <sl2424@mail.ru>
2021-06-14 02:30:38 +0300

    Add solutions for 2 tasks

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+const Text = ({size, underline, name, color}) => {
+  return <div className= "text" style={{color:color, fontSize:size, textDecoration: underline? 'underline': 'none'}}>{name}</div>
+}
+
+const Square = ({label, cssClass}) => {
+  return <div className = {cssClass}>{label}</div>
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className= "text-container">
+    <Text name="Regular text"  />
+    <Text name="Huge header" size="16px"/>
+    <Text name="Danger notification" color="red" />
+    <Text name="Underline text" underline={true} />
+  </div> 
+
+  <div className= "square-container">
+    <Square label="BIG square" cssClass='big-square'/>
+    <Square label="MIDDLE square" cssClass="middle-square"/>
+    <Square label="SMALL square" cssClass="small-square"/>
+  </div> 
   </React.StrictMode>,
   document.getElementById('root')
 );
