ed7eee3794451187c374acbe5db471a3191fecd8
24041997 <sl2424@mail.ru>
2021-07-13 21:15:53 +0300

    csa rdy

--- src/UserList.js
+++ src/UserList.js
@@ -7,26 +7,27 @@ class UserList extends Component {
 state = {
     users: []
 }
-
-
     componentDidMount(){
         axios.get('https://serverless-backend-ky9b8rmuq.now.sh/api/users').then((response)=> {
             this.setState({users: response.data})
         });
     }
 
-
+    
     render() {
-
-
+        
     return (
-    <div>
-       
-    </div>
-
+        <div>
+            {this.state.users.map((user) =>{
+                return (
+                <div key={user._id}>
+                    <h4>{user.name.first}</h4>
+                    <h5>{user.name.last}</h5>  
+                 </div>
+                )
+            })}
+        </div>
     )
 }
-
 }
-
-export default UserList;
+export default UserList
