e432fb2777a8b8fa42a3ca866f97b2eb5d17dfdf
24041997 <sl2424@mail.ru>
2021-07-23 01:54:19 +0300

    task 9.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const TimePicker = () => {
+  let [hours, setHours] = useState(0);
+  let [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    if (hours > 23) {
+      setHours((hours = 0));
+    }
+    if (hours < 0) {
+      setHours((hours = 23));
+    }
+    if (minutes == 59) {
+      setMinutes((minutes = 0));
+      setHours(hours + 1);
+    }
+    if (minutes < 0) {
+      setMinutes((minutes = 0));
+    }
+  }, [hours, minutes]);
+
+  const timerPlusHour = (event) => {
+    setHours(hours + 1);
+  };
+
+  const timerMinusHour = (event) => {
+    setHours(hours - 1);
+  };
+
+  const timerPlusMinute = (event) => {
+    setMinutes(minutes + 1);
+  };
+
+  const timerMinusMinute = (event) => {
+    setMinutes(minutes - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{hours}</div>
+      <button name="plusHour" onClick={timerPlusHour}>
+        +
+      </button>
+      <button name="minusHour" onClick={timerMinusHour}>
+        -
+      </button>
+      <div>{minutes}</div>
+      <button name="plusMinute" onClick={timerPlusMinute}>
+        +
+      </button>
+      <button name="minusMinute" onClick={timerMinusMinute}>
+        -
+      </button>
+    </>
   );
-}
-
-export default App;
+};
+export default TimePicker;
