1b31c7213c43ff4f870c3419c9d138e17689dabd
24041997 <sl2424@mail.ru>
2021-07-27 00:22:50 +0300

    add h9.1

---
+++ src/H9.1/h9.1.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import names from "./names.json";
+
+class PeopleList extends React.Component {
+  state = {
+    people: ["Nietzsche", "Goethe", "Descartes"],
+  };
+
+  onClick = () => {
+    const randIndex = Math.floor(Math.random() * 13);
+    const oneMoreName = names[randIndex];
+    const newPeople = [...this.state.people, oneMoreName];
+    this.setState({ people: newPeople });
+  };
+
+  render() {
+    console.log(this.state.people)
+    return (
+      <>
+        <button onClick={this.onClick}>Add Name</button>
+            {this.state.people.map((name) => {
+              return (
+                <div key={name.index}>
+                  <h3>{name}</h3>
+                </div>
+              )
+            })}
+      </>
+    );
+  }
+}
+export default PeopleList;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router'
+import PeopleList from './H9.1'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router />
+    <PeopleList />
   </React.StrictMode>,
   document.getElementById('root')
 );
