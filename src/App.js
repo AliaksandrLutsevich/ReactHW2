6defa8a5fe834eab7e77c2a4ca7b05db675e9575
24041997 <sl2424@mail.ru>
2021-07-30 01:01:07 +0300

    add unique id

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import names from "./names.json";
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends React.Component {
   state = {
-    people: ["Nietzsche", "Goethe", "Descartes"],
+    people: [{name: "Nietzsche"}, {name: "Goethe"}, {name: "Descartes"}],
   };
 
   onClick = () => {
     const randIndex = Math.floor(Math.random() * 13);
-    const oneMoreName = names[randIndex];
+    const oneMoreName = {
+      name: names[randIndex],
+      id: uuidv4(),
+    }
     const newPeople = [...this.state.people, oneMoreName];
     this.setState({ people: newPeople });
   };
@@ -21,10 +25,12 @@ class App extends React.Component {
         <button onClick={this.onClick}>Add Name</button>
             {this.state.people.map((name) => {
               return (
-                <div key={name.index}>
-                  <h3>{name}</h3>
+                <div key={name.id}>
+                  <h3>{name.name}</h3>
                 </div>
+
               )
+              
             })}
       </>
     );
